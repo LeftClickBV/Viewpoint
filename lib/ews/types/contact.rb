1c83c38bd6d770bce37f090eab86b1aa4f80ddbd
Johan Smits <johan.smits@leftclick.eu>
2019-09-20 13:28:21 +0200

    Add more attributes to the contact item

--- lib/ews/types/contact.rb
+++ lib/ews/types/contact.rb
@@ -5,14 +5,33 @@ module Viewpoint::EWS::Types
     include Viewpoint::EWS::Types::Item
 
     CONTACT_KEY_PATHS = {
-      contact_source: [:contact_source, :text],
-      display_name: [:display_name, :text],
-      email_addresses: [:email_addresses, :elems],
+      given_name:         [:given_name, :text],
+      surname:            [:surname, :text],
+      display_name:       [:display_name, :text],
+      company_name:       [:company_name, :text],
+      email_addresses:    [:email_addresses, :elems],
+      physical_addresses: [:physical_addresses, :elems],
+      phone_numbers:      [:phone_numbers, :elems],
+      department:         [:department, :text],
+      job_title:          [:job_title, :text],
+      office_location:    [:office_location, :text],
     }
+
     CONTACT_KEY_TYPES = {
-      email_addresses: :build_email_addresses,
+      email_addresses:    :build_email_addresses,
+      physical_addresses: :build_physical_addresses,
+      phone_numbers:      :build_phone_numbers
     }
-    CONTACT_KEY_ALIAS = {}
+
+    CONTACT_KEY_ALIAS = {
+      first_name:      :given_name,
+      last_name:       :surname,
+      emails:          :email_addresses,
+      addresses:       :physical_addresses,
+      title:           :job_title,
+      company:         :company_name,
+      office:          :office_location,
+    }
 
     def initialize(ews, contact)
       @ews = ews
@@ -38,22 +57,45 @@ module Viewpoint::EWS::Types
       end
     end
 
-    def build_email_addresses(email_addresses)
-      return [] if email_addresses.nil?
-      email_addresses.collect{|e| e[:entry][:text]}
+    def build_phone_number(phone_number)
+      Types::PhoneNumber.new(ews, phone_number)
+    end
+
+    def build_phone_numbers(phone_numbers)
+      return [] if phone_numbers.nil?
+      phone_numbers.collect { |pn| build_phone_number(pn[:entry]) }
+    end
+
+    def build_email_address(email)
+      Types::EmailAddress.new(ews, email)
+    end
+
+    def build_email_addresses(users)
+      return [] if users.nil?
+      users.collect { |u| build_email_address(u[:entry]) }
+    end
+
+    def build_physical_address(address_ews)
+      Types::PhysicalAddress.new(ews, address_ews)
+    end
+
+    def build_physical_addresses(addresses)
+      return [] if addresses.nil?
+      addresses.collect { |a| build_physical_address(a[:entry]) }
     end
 
     def key_paths
-      @key_paths ||= CONTACT_KEY_PATHS
+      super.merge(CONTACT_KEY_PATHS)
     end
 
     def key_types
-      @key_types ||= CONTACT_KEY_TYPES
+      super.merge(CONTACT_KEY_TYPES)
     end
 
     def key_alias
-      @key_alias ||= CONTACT_KEY_ALIAS
+      super.merge(CONTACT_KEY_ALIAS)
     end
+
 
   end
 end
